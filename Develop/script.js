c6fb0a436931a48311235a0efb038a601f8ac5b6
Cayman Gill <cayman.g@hotmail.com>
2021-07-04 15:38:04 -0400

    replaced window prompts and confirms for browser checkboxes andstyles

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment code here
+
 function random(arry){
-  var ranIndex = Math.floor(Math.random() * arry.length) + 1;
+  var ranIndex = Math.floor(Math.random() * arry.length);
   return ranIndex;
 }
 
@@ -11,49 +12,45 @@ function generatePassword(){
     number: ["0","1","2","3","4","5","6","7","8","9"],
     lowerCase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","p","w","x","y","z"],
     upperCase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","P","W","X","Y","Z"],
-    specialChars: ["!","@","#","$","%","^","&","*","?","<",">","+"]
+    specialChars: ["!","@","#","$","%","^","&","*","?","<",">","+","'","(",")","*",",","-",".","/",":",":","=","[","]","_","`","~","{","}","|"]
   }
   
-  var passLength = window.prompt("Enter a number for the length of the password. \n(must be more than 8 and less than 128)");
-  passLength = parseInt(passLength);
+  var confirmNumber = document.getElementById("numbers").checked;
+  var confirmLowercase = document.getElementById("lowercase").checked;
+  var confrimUppercase = document.getElementById("uppercase").checked;
+  var confrimSymbol = document.getElementById("special").checked;
 
-  while (passLength < 8 || passLength > 128){
-    passLength = window.prompt("Number entered is out of range. Please enter a number between 8 and 128.")
-  }
 
-  var confirmNumber = window.confirm("Include numbers?");
-  var confirmLowercase = window.confirm("Include Lowercase?");
-  var confrimUppercase = window.confirm("Include Uppercase?");
-  var confrimSymbol = window.confirm("Include Special Characters?");
+  var passLength = document.getElementById("length").value;
+  console.log(passLength);
 
-  
-
-  for (i = password.length + 1; i < passLength; i++){
+  console.log(passLength)
+  for (i = password.length; i < passLength; i++){
     var ranParam = Math.floor(Math.random() * 4) + 1;
-    console.log("Random Param: " + ranParam);
+    console.log(ranParam)
 
     switch(ranParam) {
       case 1:
-        console.log("Number");
+        
         if (confirmNumber){
           password.push(params.number[random(params.number)]);
         }
         break;
       case 2:
-        console.log("Lowercase");
+        console.log(confirmLowercase);
         if (confirmLowercase){
           password.push(params.lowerCase[random(params.lowerCase)]);
 
         }
         break;
       case 3:
-        console.log("Uppercase");
+        console.log(confrimUppercase);
         if (confrimUppercase){
           password.push(params.upperCase[random(params.upperCase)]);
         }
         break;
       case 4:
-        console.log("Symbol")
+        console.log(confrimSymbol)
         if (confrimSymbol){
           password.push(params.specialChars[random(params.specialChars)]);
         }
